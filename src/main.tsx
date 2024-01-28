fad5329bce49508d55517c44598e0f4d30067264
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-28 17:54:46 -0500

    refactor: change page state to use query parameters

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import App from './App';
 import Layout from './Layout';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <BrowserRouter>
-    <React.StrictMode>
-      <Layout>
-        <App />
-      </Layout>
-    </React.StrictMode>
+    <QueryParamProvider adapter={ReactRouter6Adapter}>
+      <React.StrictMode>
+        <Layout>
+          <App />
+        </Layout>
+      </React.StrictMode>
+    </QueryParamProvider>
   </BrowserRouter>,
 );
