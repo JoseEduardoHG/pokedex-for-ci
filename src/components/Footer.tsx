143556a6665773aa8335851a8e61a6d002b3b146
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-27 19:18:51 -0500

    feat: add footer component without styles

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className=''>
+      <p>Released under MIT License</p>
+      <p>Copyright ©️ 2024 JoseEduardoHG</p>
+    </footer>
+  );
+}
