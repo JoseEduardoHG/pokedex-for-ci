6d26513f3db68513a29c5dabf71dc535e52d080d
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-28 15:46:43 -0500

    refactor: change the way to get the pokemon list to properly use react-paginate

--- src/pages/PokedexPage.tsx
+++ src/pages/PokedexPage.tsx
@@ -6,66 +6,72 @@ import ReactPaginate from 'react-paginate';
 const api = new PokemonClient();
 
 export default function Pokedex() {
-  const [totalOfPages, setTotalOfPages] = useState(0);
-  // const [next, setNext] = useState<string | null>(null);
-  // const [previous, setPrevious] = useState<string | null>(null);
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
+  const [error, setError] = useState<string | null>(null);
+
+  const itemsPerPage = 10;
 
   useEffect(() => {
-    getPokemons()
-      .then()
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+    api
+      .getPokemonList((currentPage - 1) * itemsPerPage, itemsPerPage)
+      .then((resourceList) => {
+        setPageCount(Math.ceil(resourceList.count / itemsPerPage));
 
-  async function getPokemons(page = 1) {
-    const limit = 10;
-    const offset = (page - 1) * limit;
-    const resources = await api.getPokemonList(offset, limit);
-    setTotalOfPages(Math.ceil(resources.count / limit));
-    // setNext(resources.next);
-    // setPrevious(resources.previous);
-    setPokemons(
-      await Promise.all(
-        resources.results.map(
-          async (resource) => await api.getPokemon(resource.name),
-        ),
-      ),
-    );
-  }
-
-  function handlePageClick(data: { selected: number }) {
-    getPokemons(data.selected + 1)
-      .then()
-      .catch((err) => {
-        console.log(err);
+        return Promise.all(
+          resourceList.results.map(
+            async (resource) => await api.getPokemon(resource.name),
+          ),
+        );
+      })
+      .then((pokemons) => {
+        setPokemons(pokemons);
+      })
+      .catch((err: Error) => {
+        setError(err.message);
       });
-  }
+  }, [currentPage]);
 
   return (
     <>
-      <article className='white mx-2 flex flex-wrap justify-center gap-4 bg-slate-800'>
-        {pokemons.map((pokemon) => (
-          <Pokecard key={pokemon.id} pokemon={pokemon} />
-        ))}
-      </article>
-      <ReactPaginate
-        pageCount={totalOfPages}
-        previousLabel='<<'
-        nextLabel='>>'
-        breakLabel='...'
-        marginPagesDisplayed={1}
-        pageRangeDisplayed={1}
-        onPageChange={handlePageClick}
-        containerClassName='pagination'
-        disableInitialCallback={true}
-        pageLinkClassName='page-item'
-        breakLinkClassName='page-item'
-        previousLinkClassName='controls'
-        nextLinkClassName='controls'
-        activeLinkClassName='active'
-      />
+      {error ? (
+        <section className='text-red-500'>{error}</section>
+      ) : (
+        <section className='flex flex-col justify-center gap-4'>
+          <section className='white mx-2 flex flex-wrap justify-center gap-4 bg-slate-800'>
+            {pokemons.map((pokemon) => (
+              <Pokecard key={pokemon.id} pokemon={pokemon} />
+            ))}
+          </section>
+          <ReactPaginate
+            pageCount={pageCount}
+            previousLabel='<'
+            nextLabel='>'
+            breakLabel='...'
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={1}
+            onPageChange={(selectedItem) => {
+              setCurrentPage(selectedItem.selected + 1);
+              // console.table({ onPageChange: selectedItem.selected });
+            }}
+            // onPageActive={(selectedItem) => {
+            //   // setCurrentPage(selectedItem.selected + 1);
+            //   console.table({ onPageActive: selectedItem.selected });
+            // }}
+            // onClick={(selectedItem) => {
+            //   console.table({ onClick: selectedItem.selected });
+            // }}
+            containerClassName='pagination'
+            disableInitialCallback={true}
+            pageLinkClassName='page-item'
+            breakLinkClassName='page-item'
+            previousLinkClassName='controls'
+            nextLinkClassName='controls'
+            activeLinkClassName='active'
+          />
+        </section>
+      )}
     </>
   );
 }
