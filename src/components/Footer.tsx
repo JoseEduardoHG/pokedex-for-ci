6fbd7df75973ffc6d5777eb615c7d05218172151
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-27 19:41:19 -0500

    feat: add styles to footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,20 @@
 export default function Footer() {
   return (
-    <footer className=''>
-      <p>Released under MIT License</p>
-      <p>Copyright ©️ 2024 JoseEduardoHG</p>
+    <footer className='flex flex-col items-center gap-2 bg-slate-900 px-8 py-4'>
+      <p className='text-2xl font-bold'>Copyright ©️ 2024 JoseEduardoHG</p>
+      <p className='text-2xl'>Released under MIT License</p>
+      <p className='flex gap-2 text-2xl'>
+        You can see the source code on{' '}
+        <span className='font-bold text-blue-200'>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://github.com/JoseEduardoHG/pokedex-for-ci'
+          >
+            GitHub
+          </a>
+        </span>
+      </p>
     </footer>
   );
 }
