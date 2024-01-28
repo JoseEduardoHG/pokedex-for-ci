f091a9b5cb6982785f6f206547ff1033b6d697d0
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-27 22:40:31 -0500

    feat: add pagination to pokedex and its styles

--- src/pages/PokedexPage.tsx
+++ src/pages/PokedexPage.tsx
@@ -1,10 +1,14 @@
 import Pokecard from '@/components/Pokecard';
 import { Pokemon, PokemonClient } from '@/services/pokeapi';
 import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 const api = new PokemonClient();
 
 export default function Pokedex() {
+  const [totalOfPages, setTotalOfPages] = useState(0);
+  // const [next, setNext] = useState<string | null>(null);
+  // const [previous, setPrevious] = useState<string | null>(null);
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
@@ -15,8 +19,13 @@ export default function Pokedex() {
       });
   }, []);
 
-  async function getPokemons() {
-    const resources = await api.getPokemonList();
+  async function getPokemons(page = 1) {
+    const limit = 10;
+    const offset = (page - 1) * limit;
+    const resources = await api.getPokemonList(offset, limit);
+    setTotalOfPages(Math.ceil(resources.count / limit));
+    // setNext(resources.next);
+    // setPrevious(resources.previous);
     setPokemons(
       await Promise.all(
         resources.results.map(
@@ -26,11 +35,37 @@ export default function Pokedex() {
     );
   }
 
+  function handlePageClick(data: { selected: number }) {
+    getPokemons(data.selected + 1)
+      .then()
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
-    <article className='white mx-2 flex flex-wrap justify-center gap-4 bg-slate-800'>
-      {pokemons.map((pokemon) => (
-        <Pokecard key={pokemon.id} pokemon={pokemon} />
-      ))}
-    </article>
+    <>
+      <article className='white mx-2 flex flex-wrap justify-center gap-4 bg-slate-800'>
+        {pokemons.map((pokemon) => (
+          <Pokecard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </article>
+      <ReactPaginate
+        pageCount={totalOfPages}
+        previousLabel='<<'
+        nextLabel='>>'
+        breakLabel='...'
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={1}
+        onPageChange={handlePageClick}
+        containerClassName='pagination'
+        disableInitialCallback={true}
+        pageLinkClassName='page-item'
+        breakLinkClassName='page-item'
+        previousLinkClassName='controls'
+        nextLinkClassName='controls'
+        activeLinkClassName='active'
+      />
+    </>
   );
 }
