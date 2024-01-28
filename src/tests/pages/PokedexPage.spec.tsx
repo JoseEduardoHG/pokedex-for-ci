fb1e861ec528f40c864b988c9da01fc5ea7a655a
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-28 17:53:30 -0500

    test: mock app and PokedexPage components

--- src/tests/pages/PokedexPage.spec.tsx
+++ src/tests/pages/PokedexPage.spec.tsx
@@ -1,10 +1,20 @@
 import PokedexPage from '@/pages/PokedexPage';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryParamProvider } from 'use-query-params';
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { describe, it } from 'vitest';
 
+const MockPokedexPage = () => (
+  <BrowserRouter>
+    <QueryParamProvider adapter={ReactRouter6Adapter}>
+      <PokedexPage />
+    </QueryParamProvider>
+  </BrowserRouter>
+);
+
 describe('PokedexPage', () => {
-  it('should render a list of Pokecards', () => {
-    render(<PokedexPage />);
-    // await screen.findAllByTestId('pokemon-card');
+  it('should render', () => {
+    render(<MockPokedexPage />);
   });
 });
