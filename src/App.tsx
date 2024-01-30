9bdb027ae56574d1b04ca1fc6b77e61c2782a938
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 16:07:42 -0500

    feat: add routes for PokemonDetailPage and NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import NotFoundPage from '@/pages/NotFoundPage';
 import PokedexPage from '@/pages/PokedexPage';
+import PokemonDetailPage from '@/pages/PokemonDetailPage';
 import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<PokedexPage />} />
+      <Route path='/pokemon/:pokemonId' element={<PokemonDetailPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
 }
