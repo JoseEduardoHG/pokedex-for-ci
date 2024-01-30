dfbe88f7a246e619b5df2de1a1a3e4ca4e9cd34c
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 14:49:46 -0500

    test: refactor the PokemonCard and PokemonList components

--- src/tests/components/PokemonList.spec.tsx
+++ src/tests/components/PokemonList.spec.tsx
@@ -1,23 +1,7 @@
 import PokemonList from '@/components/PokemonList';
 import { Pokemon } from '@/services/pokeapi';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryParamProvider } from 'use-query-params';
-import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { describe, expect, it } from 'vitest';
-
-interface MockPokemonListProps {
-  pokemons: Pokemon[];
-  onClick?: (pokemonId: number) => void;
-}
-
-const MockPokemonList = ({ pokemons, onClick }: MockPokemonListProps) => (
-  <BrowserRouter>
-    <QueryParamProvider adapter={ReactRouter6Adapter}>
-      <PokemonList pokemons={pokemons} onClick={onClick} />
-    </QueryParamProvider>
-  </BrowserRouter>
-);
 
 const pokemons: Pokemon[] = [
   {
@@ -36,12 +20,12 @@ const pokemons: Pokemon[] = [
 
 describe('PokemonList', () => {
   it('should render a grid', () => {
-    render(<MockPokemonList pokemons={pokemons} />);
+    render(<PokemonList pokemons={pokemons} />);
     screen.getByRole('grid');
   });
 
   it('should render a list of pokemons', () => {
-    render(<MockPokemonList pokemons={pokemons} />);
+    render(<PokemonList pokemons={pokemons} />);
     const elements = screen.getAllByRole('cell');
     expect(elements).toHaveLength(2);
   });
