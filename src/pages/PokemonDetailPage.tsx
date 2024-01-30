4482ecb3e947c80117da88aead80e7e7dd5b41ca
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 16:07:06 -0500

    feat: add PokemonDetailPage and NotFoundPage

---
+++ src/pages/PokemonDetailPage.tsx
@@ -0,0 +1,18 @@
+export default function PokemonDetailPage() {
+  return (
+    <div>
+      <p>No. 1</p>
+      <h2>Bulbasur</h2>
+      <p>
+        Tras nacer, crece alimentándose durante un tiempo de los nutrientes que
+        contiene el bulbo de su lomo.
+      </p>
+      <p>0.7 m</p>
+      <p>6.9 kg</p>
+      <p>Seed</p>
+      <p>Espesura</p>
+      <p data-testid={'type'}>Planta</p>
+      <p data-testid='debility'>Fire</p>
+    </div>
+  );
+}
