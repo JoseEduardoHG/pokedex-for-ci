920132af95fd57e3d65b380e6dae9c03a6443e68
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 16:51:17 -0500

    feat: add styles for button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,5 +4,12 @@ interface ButtonProps {
 }
 
 export default function Button({ text, onClick }: ButtonProps) {
-  return <button onClick={onClick}>{text}</button>;
+  return (
+    <button
+      className='rounded-lg bg-yellow-300 px-4 py-2 text-lg font-bold text-slate-800 sm:w-fit'
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
