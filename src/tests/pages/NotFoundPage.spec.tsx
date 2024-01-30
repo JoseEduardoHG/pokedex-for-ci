e575e3afd5cca2dccb7684a1b3ffe0c0216acd07
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 17:01:52 -0500

    test: mock NotFoundPage to use BrowserRouter

--- src/tests/pages/NotFoundPage.spec.tsx
+++ src/tests/pages/NotFoundPage.spec.tsx
@@ -1,20 +1,27 @@
 import NotFoundPage from '@/pages/NotFoundPage';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import { describe, it } from 'vitest';
+
+const MockNotFoundPage = () => (
+  <BrowserRouter>
+    <NotFoundPage />
+  </BrowserRouter>
+);
 
 describe('NotFoundPage', () => {
   it('should render page not found', () => {
-    render(<NotFoundPage />);
+    render(<MockNotFoundPage />);
     screen.getByText(/page not found/i);
   });
 
   it('should render a little message', () => {
-    render(<NotFoundPage />);
+    render(<MockNotFoundPage />);
     screen.getByText(/you should check if the url provided was correct/i);
   });
 
   it('should render a button that says go to home', () => {
-    render(<NotFoundPage />);
+    render(<MockNotFoundPage />);
     screen.getByRole('button', { name: /go to home/i });
   });
 });
