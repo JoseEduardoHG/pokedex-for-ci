a5dfbfd1682ab8c22ce0e4205c457d075fb3cd72
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 16:39:33 -0500

    test: fix test description

--- src/tests/components/Button.spec.tsx
+++ src/tests/components/Button.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
 
 describe('Button', () => {
-  it('should render the text passed as prop', () => {
+  it('should render the text if passed as prop', () => {
     render(<Button text='Foo' />);
     screen.getByRole('button', { name: /foo/i });
   });
