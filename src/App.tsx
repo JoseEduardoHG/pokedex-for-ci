f9e7c51588ef8fab011f41ab51b09b99ce526cba
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 18:36:19 -0500

    refactor: change url parameter name for PokemonDetailPage

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <Routes>
       <Route path='/' element={<PokedexPage />} />
-      <Route path='/pokemon/:pokemonId' element={<PokemonDetailPage />} />
+      <Route path='/pokedex/:pokemonName' element={<PokemonDetailPage />} />
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
