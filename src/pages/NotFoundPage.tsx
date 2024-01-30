beb0852b14362722bd954376c96e5183c5834bdf
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 17:02:10 -0500

    feat: add styles to not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,9 +1,20 @@
+import Button from '@/components/Button';
+import { useNavigate } from 'react-router-dom';
+
 export default function NotFoundPage() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/', { replace: true });
+  }
+
   return (
-    <section>
-      <h1>Page Not Found</h1>
-      <p>You should check if the URL provided was correct.</p>
-      <button>Go to home</button>
+    <section className='mx-4 flex flex-col gap-4'>
+      <h1 className='text-3xl font-bold sm:text-6xl'>Page Not Found</h1>
+      <p className='text-lg'>
+        You should check if the URL provided was correct.
+      </p>
+      <Button text='Go to home' onClick={handleClick} />
     </section>
   );
 }
