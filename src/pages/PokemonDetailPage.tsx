b3427959c050eda998c43ee8b5ce6ec678b0a853
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 17:20:30 -0500

    feat: add PokemonDetailPage

--- src/pages/PokemonDetailPage.tsx
+++ src/pages/PokemonDetailPage.tsx
@@ -1,18 +1,48 @@
+import ErrorDisplay from '@/components/ErrorDisplay';
+import { Pokemon, PokemonClient } from '@/services/pokeapi';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function PokemonDetailPage() {
+  const { pokemonId } = useParams<{ pokemonId: string }>();
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (pokemonId) {
+      const api = new PokemonClient();
+
+      api
+        .getPokemon(pokemonId)
+        .then((pokemon) => {
+          setPokemon(pokemon);
+        })
+        .catch((err: Error) => {
+          console.error(err);
+        });
+    }
+  }, [pokemonId]);
+
   return (
-    <div>
-      <p>No. 1</p>
-      <h2>Bulbasur</h2>
-      <p>
-        Tras nacer, crece alimentándose durante un tiempo de los nutrientes que
-        contiene el bulbo de su lomo.
-      </p>
-      <p>0.7 m</p>
-      <p>6.9 kg</p>
-      <p>Seed</p>
-      <p>Espesura</p>
-      <p data-testid={'type'}>Planta</p>
-      <p data-testid='debility'>Fire</p>
-    </div>
+    <>
+      {error ? (
+        <ErrorDisplay error={error} />
+      ) : (
+        <>
+          <p>No. 1</p>
+          <h2>Bulbasur</h2>
+          <p>
+            Tras nacer, crece alimentándose durante un tiempo de los nutrientes
+            que contiene el bulbo de su lomo.
+          </p>
+          <p>0.7 m</p>
+          <p>6.9 kg</p>
+          <p>Seed</p>
+          <p>Espesura</p>
+          <p data-testid={'type'}>Planta</p>
+          <p data-testid='debility'>Fire</p>
+        </>
+      )}
+    </>
   );
 }
