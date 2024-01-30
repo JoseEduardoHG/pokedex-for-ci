2fd806d5d493ea57679b1dcf790326526eed31f8
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 13:52:35 -0500

    feat: change pill padding to 1px

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -4,6 +4,6 @@ interface PillProps {
 
 export default function Pill({ name }: PillProps) {
   return (
-    <span className='w-fit rounded-full bg-slate-500 px-4 py-2'>{name}</span>
+    <span className='w-fit rounded-full bg-slate-500 px-4 py-1'>{name}</span>
   );
 }
