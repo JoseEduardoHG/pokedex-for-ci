72b753c192b31a0a87ebeead2ab526813391d1d7
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-30 09:38:39 -0500

    refactor: change the name of the PokeCard to PokemonCard

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { FaExternalLinkAlt } from 'react-icons/fa';
+
 export default function Footer() {
   return (
     <footer className='flex flex-col items-center gap-2 bg-slate-900 px-8 py-4'>
@@ -5,13 +7,17 @@ export default function Footer() {
       <p className='text-2xl'>Released under MIT License</p>
       <p className='flex flex-col gap-2 text-2xl sm:flex-row'>
         You can see the source code on{' '}
-        <span className='font-bold text-yellow-300'>
+        <span className='font-bold text-yellow-300 underline'>
           <a
             target='_blank'
             rel='noopener noreferrer'
             href='https://github.com/JoseEduardoHG/pokedex-for-ci'
+            className='flex items-center gap-2'
           >
             GitHub
+            <span>
+              <FaExternalLinkAlt size={18} />
+            </span>
           </a>
         </span>
       </p>
