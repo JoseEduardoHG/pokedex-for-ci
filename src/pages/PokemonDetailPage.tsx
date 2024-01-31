18cb9a7a5d0450c4a0b2f2bd21ada89530ebf95c
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-31 18:30:48 -0500

    feat: add more pokemon details

--- src/pages/PokemonDetailPage.tsx
+++ src/pages/PokemonDetailPage.tsx
@@ -1,10 +1,14 @@
+import Button from '@/components/Button';
 import ErrorDisplay from '@/components/ErrorDisplay';
+import Pill from '@/components/Pill';
 import { Pokemon, PokemonClient } from '@/services/pokeapi';
 import { capitalize } from '@/utils/utils';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { FaChevronLeft } from 'react-icons/fa';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function PokemonDetailPage() {
+  const navigate = useNavigate();
   const { pokemonName } = useParams<{ pokemonName: string }>();
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
   const [error, setError] = useState<string | null>(null);
@@ -24,11 +28,18 @@ export default function PokemonDetailPage() {
     }
   }, [pokemonName]);
 
+  function handleClick() {
+    navigate(-1);
+  }
+
   return (
     <>
       {error && <ErrorDisplay error={error} />}
       {pokemon && (
         <section className='mx-4 flex flex-col gap-4'>
+          <Button onClick={handleClick}>
+            <FaChevronLeft />
+          </Button>
           <p className='caption'>No. {pokemon.id}</p>
           <h2>{capitalize(pokemon.name)}</h2>
           <img
@@ -43,18 +54,39 @@ export default function PokemonDetailPage() {
           />
           {/* <p>{'description'}</p> */}
           <h3>Height</h3>
-          <p className='text-xl'>{pokemon.height / 10} m</p>
+          <p>{pokemon.height / 10} m</p>
           <h3>Weight</h3>
-          <p className='text-xl'>{pokemon.weight / 10} kg</p>
+          <p>{pokemon.weight / 10} kg</p>
+          <h3>Species</h3>
+          <p>{capitalize(pokemon.species.name)}</p>
           {/* <p>{'category'}</p> */}
-          <h3>Abilities</h3>
-          <p className='text-xl'>
-            {capitalize(pokemon.abilities[0].ability.name)}
-          </p>
           <h3>Types</h3>
-          <p className='text-xl' data-testid='type'>
-            {capitalize(pokemon.types[0].type.name)}
-          </p>
+          <section className='flex gap-2'>
+            {pokemon.types.map((pokemonType) => (
+              <Pill
+                key={pokemonType.type.name}
+                name={capitalize(pokemonType.type.name)}
+              />
+            ))}
+          </section>
+          <h3>Abilities</h3>
+          <section className='flex gap-2'>
+            {pokemon.abilities.map((pokemonAbility) => (
+              <Pill
+                key={pokemonAbility.ability.name}
+                name={capitalize(pokemonAbility.ability.name)}
+              />
+            ))}
+          </section>
+          <h3>Stats</h3>
+          <section className='flex flex-wrap gap-2'>
+            {pokemon.stats.map((pokemonStat) => (
+              <Pill
+                key={pokemonStat.stat.name}
+                name={capitalize(pokemonStat.stat.name)}
+              />
+            ))}
+          </section>
           {/* <p data-testid='debility'>{'debility'}</p> */}
         </section>
       )}
