8e2743306cac95c591bf8216b9b5458434207f83
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-01-31 19:02:12 -0500

    fix: style for pokemon detail page for computer screen

--- src/pages/PokemonDetailPage.tsx
+++ src/pages/PokemonDetailPage.tsx
@@ -36,59 +36,77 @@ export default function PokemonDetailPage() {
     <>
       {error && <ErrorDisplay error={error} />}
       {pokemon && (
-        <section className='mx-4 flex flex-col gap-4'>
-          <Button onClick={handleClick}>
-            <FaChevronLeft />
-          </Button>
-          <p className='caption'>No. {pokemon.id}</p>
-          <h2>{capitalize(pokemon.name)}</h2>
-          <img
-            src={
-              pokemon.sprites.other?.dream_world.front_default ??
-              pokemon.sprites.other?.home.front_default ??
-              pokemon.sprites.other?.official_artwork.front_default ??
-              undefined
-            }
-            alt={`${pokemon.name} image`}
-            className=''
-          />
-          {/* <p>{'description'}</p> */}
-          <h3>Height</h3>
-          <p>{pokemon.height / 10} m</p>
-          <h3>Weight</h3>
-          <p>{pokemon.weight / 10} kg</p>
-          <h3>Species</h3>
-          <p>{capitalize(pokemon.species.name)}</p>
-          {/* <p>{'category'}</p> */}
-          <h3>Types</h3>
-          <section className='flex gap-2'>
-            {pokemon.types.map((pokemonType) => (
-              <Pill
-                key={pokemonType.type.name}
-                name={capitalize(pokemonType.type.name)}
-              />
-            ))}
+        <>
+          <section className='mx-4'>
+            <Button onClick={handleClick}>
+              <FaChevronLeft />
+            </Button>
           </section>
-          <h3>Abilities</h3>
-          <section className='flex gap-2'>
-            {pokemon.abilities.map((pokemonAbility) => (
-              <Pill
-                key={pokemonAbility.ability.name}
-                name={capitalize(pokemonAbility.ability.name)}
-              />
-            ))}
+          <section className='mx-4 flex flex-col gap-4'>
+            <p className='caption mx-auto'>No. {pokemon.id}</p>
+            <h2 className='mx-auto'>{capitalize(pokemon.name)}</h2>
+            <img
+              src={
+                pokemon.sprites.other?.dream_world.front_default ??
+                pokemon.sprites.other?.home.front_default ??
+                pokemon.sprites.other?.official_artwork.front_default ??
+                undefined
+              }
+              alt={`${pokemon.name} image`}
+              className='mx-auto w-96'
+            />
+            <section className='mx-auto grid gap-8 grid-auto-fit-md'>
+              {/* <p>{'description'}</p> */}
+              <section className='flex flex-col gap-4'>
+                <h3>Height</h3>
+                <p>{pokemon.height / 10} m</p>
+              </section>
+              <section className='flex flex-col gap-4'>
+                <h3>Weight</h3>
+                <p>{pokemon.weight / 10} kg</p>
+              </section>
+              <section className='flex flex-col gap-4'>
+                <h3>Species</h3>
+                <p>{capitalize(pokemon.species.name)}</p>
+              </section>
+              {/* <p>{'category'}</p> */}
+              <section className='flex flex-col gap-4'>
+                <h3>Types</h3>
+                <section className='flex gap-2'>
+                  {pokemon.types.map((pokemonType) => (
+                    <Pill
+                      key={pokemonType.type.name}
+                      name={capitalize(pokemonType.type.name)}
+                    />
+                  ))}
+                </section>
+              </section>
+              <section className='flex flex-col gap-4'>
+                <h3>Abilities</h3>
+                <section className='flex gap-2'>
+                  {pokemon.abilities.map((pokemonAbility) => (
+                    <Pill
+                      key={pokemonAbility.ability.name}
+                      name={capitalize(pokemonAbility.ability.name)}
+                    />
+                  ))}
+                </section>
+              </section>
+              <section className='flex flex-col gap-4'>
+                <h3>Stats</h3>
+                <section className='flex flex-wrap gap-2'>
+                  {pokemon.stats.map((pokemonStat) => (
+                    <Pill
+                      key={pokemonStat.stat.name}
+                      name={capitalize(pokemonStat.stat.name)}
+                    />
+                  ))}
+                </section>
+              </section>
+              {/* <p data-testid='debility'>{'debility'}</p> */}
+            </section>
           </section>
-          <h3>Stats</h3>
-          <section className='flex flex-wrap gap-2'>
-            {pokemon.stats.map((pokemonStat) => (
-              <Pill
-                key={pokemonStat.stat.name}
-                name={capitalize(pokemonStat.stat.name)}
-              />
-            ))}
-          </section>
-          {/* <p data-testid='debility'>{'debility'}</p> */}
-        </section>
+        </>
       )}
     </>
   );
